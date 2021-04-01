fe812b782ad9f1c80467359130fd8cc2960d47da
Celine V <dandan@celines-imac.home>
2021-04-01 13:08:09 -0700

    add heroku psql

--- server.js
+++ server.js
@@ -11,11 +11,9 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: '',
-    password: '',
-    database: 'smartbrain',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
 });
 
